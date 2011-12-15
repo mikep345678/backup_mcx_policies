d44a083d13d36123bac0acbfe16564baf5a0c5de
Mike Pullen <mike@thepullen.net>
2011-12-15 15:35:59 -0600

    added usergroup backup

--- backup_mcx_policies.sh
+++ backup_mcx_policies.sh
@@ -6,7 +6,18 @@ computers=`dscl $oddomain -list ComputerGroups`
 
 for c in $computers
 do
-    outfile="~/Desktop/mcx_backup/$c.plist"
+    outfile="~/Desktop/mcx_backup/computergroups/$c.plist"
     echo $outfile
 	dscl $oddomain -mcxexport /ComputerGroups/$c -o "$outfile"
 done
+
+users=`dscl $oddomain -list Groups`
+
+for u in $users
+do
+    outfile="~/Desktop/mcx_backup/usersgroups/$u.plist"
+    echo $outfile
+	dscl $oddomain -mcxexport /Groups/$u -o "$outfile"
+done
+
+exit 0
